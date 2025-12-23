65cdaba5a54b8fe6cf9c7df58dd9ea8e7384668a
Roland Milto <34012369+roland-milto@users.noreply.github.com>
2025-12-23 12:50:19 +0100

    Tested example in README.md

--- tests/test.mjs
+++ tests/test.mjs
@@ -5,7 +5,12 @@ process.env.NODE_ENV = 'development';             // Can be *any* string to enab
 /*
 assert.integer(42.4, "my integer failure label"); // Failure, maybe you should use assert.number instead? ;)
 assert.string(54545, "my string failure label");  // Failure
- */
+
+
+let meaningOfLife = 42.4;
+assert.integer(meaningOfLife, "meaningOfLife label");
+
+*/
 
 let strings = ['Roland', 'Milto', 'from', 'Germany'];
 assert.arrayOfString(strings, 'good strings');    // Success
