a1f152632cd236568523ed8669d1474100cf0559
Roland Milto <34012369+roland-milto@users.noreply.github.com>
2025-12-23 08:51:40 +0100

    Changed "any" to "unknown"

--- src/caller/interface.ts
+++ src/caller/interface.ts
@@ -1,31 +1,32 @@
 // Import: Build-ins.
-import {Stream} from "node:stream";
+import type {Stream} from "node:stream";
 
 /**
  * Prevents TS2775 errors when these methods are used in `optionalArrayItemTypeCheck`.
  *
  * @author  Roland Milto (https://roland.milto.de/)
- * @version 2025-12-19
+ * @version 2025-12-23
  */
 export interface ArrayItemTypeCheck {
-  arrayOfArray: (argument: any, label: string) => asserts argument is any[][];
-  arrayOfBigInt: (argument: any, label: string) => asserts argument is bigint[];
-  arrayOfBool: (argument: any, label: string) => asserts argument is boolean[];
-  arrayOfBoolean: (argument: any, label: string) => asserts argument is boolean[];
-  arrayOfBuffer: (argument: any, label: string) => asserts argument is Buffer[];
-  arrayOfDate: (argument: any, label: string) => asserts argument is Date[];
-  arrayOfFinite: (argument: any, label: string) => asserts argument is number[];
-  arrayOfFunc: (argument: any, label: string) => asserts argument is Function[];
-  arrayOfFunction: (argument: any, label: string) => asserts argument is Function[];
-  arrayOfInteger: (argument: any, label: string) => asserts argument is number[];
-  arrayOfNull: (argument: any, label: string) => asserts argument is null[];
-  arrayOfNumber: (argument: any, label: string) => asserts argument is number[];
-  arrayOfObject: (argument: any, label: string) => asserts argument is object[];
-  arrayOfPlainObject: (argument: any, label: string) => asserts argument is Record<string, unknown>[];
-  arrayOfRegexp: (argument: any, label: string) => asserts argument is RegExp[];
-  arrayOfStream: (argument: any, label: string) => asserts argument is Stream[];
-  arrayOfString: (argument: any, label: string) => asserts argument is string[];
-  arrayOfSymbol: (argument: any, label: string) => asserts argument is symbol[];
-  arrayOfUndefined: (argument: any, label: string) => asserts argument is undefined[];
-  arrayOfUuid: (argument: any, label: string) => asserts argument is string[];
+  arrayOfArray: (argument: unknown, label: string) => asserts argument is unknown[][];
+  arrayOfBigInt: (argument: unknown, label: string) => asserts argument is bigint[];
+  arrayOfBool: (argument: unknown, label: string) => asserts argument is boolean[];
+  arrayOfBoolean: (argument: unknown, label: string) => asserts argument is boolean[];
+  arrayOfBuffer: (argument: unknown, label: string) => asserts argument is Buffer[];
+  arrayOfDate: (argument: unknown, label: string) => asserts argument is Date[];
+  arrayOfFinite: (argument: unknown, label: string) => asserts argument is number[];
+  arrayOfFunc: (argument: unknown, label: string) => asserts argument is Function[];
+  arrayOfFunction: (argument: unknown, label: string) => asserts argument is Function[];
+  arrayOfInteger: (argument: unknown, label: string) => asserts argument is number[];
+  arrayOfNull: (argument: unknown, label: string) => asserts argument is null[];
+  arrayOfNumber: (argument: unknown, label: string) => asserts argument is number[];
+  arrayOfObject: (argument: unknown, label: string) => asserts argument is object[];
+  arrayOfPlainObject: (argument: unknown, label: string) => asserts argument is Record<string, unknown>[];
+  arrayOfRegexp: (argument: unknown, label: string) => asserts argument is RegExp[];
+  arrayOfStream: (argument: unknown, label: string) => asserts argument is Stream[];
+  arrayOfString: (argument: unknown, label: string) => asserts argument is string[];
+  arrayOfSymbol: (argument: unknown, label: string) => asserts argument is symbol[];
+  arrayOfUndefined: (argument: unknown, label: string) => asserts argument is undefined[];
+  arrayOfUuid: (argument: unknown, label: string) => asserts argument is string[];
+  arrayOfValidDate: (argument: unknown, label: string) => asserts argument is Date[];
 }
