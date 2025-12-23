3086b0bf60375b80308d6ad9589435847207eee6
Roland Milto <34012369+roland-milto@users.noreply.github.com>
2025-12-23 12:29:58 +0100

    Inserted typeCheck

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // Import: Self-created functions.
-import {booleanCheck} from "./caller/booleanCheck.ts";
+import {typeCheck} from "./caller/typeCheck.js";
 import {optionalTypeCheck} from "./caller/optionalTypeCheck.js";
 import {arrayItemTypeCheck} from "./caller/arrayItemTypeCheck.js";
 import {optionalArrayItemTypeCheck} from "./caller/optionalArrayItemTypeCheck.js";
@@ -8,10 +8,10 @@ import {optionalArrayItemTypeCheck} from "./caller/optionalArrayItemTypeCheck.js
  * Generates assertion methods statically for maximum performance and type safety.
  *
  * @author  Roland Milto (https://roland.milto.de/)
- * @version 2025-12-19
+ * @version 2025-12-23
  */
 export const assert = {
-  ...booleanCheck,
+  ...typeCheck,
   ...optionalTypeCheck,
   ...arrayItemTypeCheck,
   ...optionalArrayItemTypeCheck
