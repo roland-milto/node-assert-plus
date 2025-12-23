bcba79ca20bcd858c4ac0588a8199b9afdf42609
Roland Milto <34012369+roland-milto@users.noreply.github.com>
2025-12-23 12:50:33 +0100

    Changed function signature

--- src/utils/toss.ts
+++ src/utils/toss.ts
@@ -8,20 +8,20 @@ import {AssertionError} from 'assert';
  *          Joyent, Inc. and assert-plus authors
  * @version 2025-12-18
  *
- * @param   {string} label                    - The name of the parameter or condition being validated.
- * @param   {string} expected                 - The expected value or type for the parameter.
- * @param   {any} arg                         - The actual value of the parameter that is being checked.
- * @param   {string} [operator='===']         - The comparison operator used to validate the argument.
- * @param   {(a: any) => string} [actualFn]   - A function to evaluate and return a string representation of the actual value.
+ * @param   {string} label                      - The name of the parameter or condition being validated.
+ * @param   {string} expected                   - The expected value or type for the parameter.
+ * @param   {unknown} argument                  - The actual value of the parameter that is being checked.
+ * @param   {string} [operator='===']           - The comparison operator used to validate the argument.
+ * @param   {(a: unknown) => string} [actualFn] - A function to evaluate and return a string representation of the actual value.
  *
- * @returns {never}                           - This method does not return a value and always throws an error.
+ * @returns {never}                             - This method does not return a value and always throws an error.
  */
-export function toss(label: string, expected: string, arg: any, operator: string = '===', actualFn?: (a: any) => string): never
+export function toss(label: string, expected: string, argument: unknown, operator: string = '===', actualFn?: (a: unknown) => string): never
 {
   throw new AssertionError(
   {
     message: `At "${label}" type of "${expected}" is required.`,
-    actual: actualFn ? actualFn(arg) : typeof arg,
+    actual: actualFn ? actualFn(argument) : typeof argument,
     expected: expected,
     operator: operator
   });
