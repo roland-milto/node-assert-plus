0cbae0ada38f71888c25df8ab3f6d6986e4ead43
Roland Milto <34012369+roland-milto@users.noreply.github.com>
2026-01-14 10:15:47 +0100

    Froze object to prevent accidental modifications to the "assert" namespace.

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,21 @@
 import nodeAssert from "node:assert/strict";
 
 // Import: Self-created functions.
-import {typeCheck} from "./caller/typeCheck.js";
-import {optionalTypeCheck} from "./caller/optionalTypeCheck.js";
-import {arrayItemTypeCheck} from "./caller/arrayItemTypeCheck.js";
-import {optionalArrayItemTypeCheck} from "./caller/optionalArrayItemTypeCheck.js";
+import { typeCheck } from "./caller/typeCheck.js";
+import { optionalTypeCheck } from "./caller/optionalTypeCheck.js";
+import { arrayItemTypeCheck } from "./caller/arrayItemTypeCheck.js";
+import { optionalArrayItemTypeCheck } from "./caller/optionalArrayItemTypeCheck.js";
 
 /**
  * Generates assertion methods statically for maximum performance and type safety.
  *
  * @author  Roland Milto (https://roland.milto.de/)
- * @version 2025-12-23
+ * @version 2025-01-14
  */
-export const assert = {
+export const assert = Object.freeze({
+  ...nodeAssert,
   ...typeCheck,
   ...optionalTypeCheck,
   ...arrayItemTypeCheck,
   ...optionalArrayItemTypeCheck,
-  ...nodeAssert
-};
+});
